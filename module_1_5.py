21ae1ef58774db41db358eabfd6b2c9dca2a0a90
LeMyt <72269801+LeMyt@users.noreply.github.com>
2024-08-31 08:47:22 +0500

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,6 @@
+immutable_var = (1, 2, 3, 'q', 'w', 'e', True)
+print('immutable_var:', immutable_var)
+#immutable_var[0] = 5 - ошибка
+mutable_list = ['a', 's', 'd', 9, 8, 7]
+mutable_list[2] = 0
+print('mutable_list:', mutable_list)
